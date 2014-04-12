d40e2d7eb8a264a1ccc2188bc8ffa9b10d083e50
sophia <scastellarin95@gmail.co>
2014-04-12 12:34:33 -0400

    all setup for flask

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+setup(name='astro_tweet_bot',
+      version='0.0.0.0.0.1',
+      description='a twitter bot for astornomical events.  It\s out of this world',
+      author='Sophia Castellarin, Steven Wu, Alan Wu',
+      author_email='binge.coders@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Flask==0.7.2', 'BeautifulSoup==3.2.1'],
      )
