877a60918285467d2632483a4bf7c02a2990e17a
XrXr <xrsquared@gmail.com>
2014-04-13 07:27:31 -0400

    Scraped data to tweet

---
+++ scraper/scraped_to_tweet.py
@@ -0,0 +1,38 @@
+import random
+TEMPLATES = [ "You could see %s in %s, ",
+              "If you look up, you might see %s in the %s constellation, ",
+              "Look for %s in the %s constellation, ",
+              "%s is above you in the %s constellation, you could also find ",
+              "You might find %s in %s constellation, "
+            ]
+
+def info_to_tweet(sightings):
+    # expects a list of dicts
+    template = TEMPLATES[random.randrange(len(TEMPLATES))]
+    result = ""
+    while True:
+        result = template % (sightings[0]['primary_catalog'], sightings[0]['constellation'])
+        for c in range(1, len(sightings)):
+            if c == len(sightings) - 1:
+                continue
+            else:
+                result += ", %s in %s" % (sightings[c]['primary_catalog'], sightings[c]['constellation'])
+        else:
+            if c >= 1 and template != TEMPLATES[3]:
+                result += "and %s in %s" % (sightings[c]['primary_catalog'], sightings[c]['constellation'])
+            else:
+                result += "%s in %s" % (sightings[c]['primary_catalog'], sightings[c]['constellation'])
+
+        if len(result) <= 140:
+            break
+        else:
+            sightings.pop(len(sightings) - 1)
+    return result
+
+if __name__ == '__main__':
+    print info_to_tweet(
+        [{'primary_catalog': 'Jupiter',
+          'constellation': 'Gemini'},
+         {'primary_catalog': 'Mars',
+          'constellation': 'Cactus'}]
+    )
