ae569b3e61bf46a5fb666969b82d6850143d1284
sophia <scastellarin95@gmail.co>
2014-04-12 22:20:38 -0400

    make api wireframe
    still need to attach to scraper service

---
+++ wsgi/api/request.py
@@ -0,0 +1,41 @@
+'''
+Module that scrapes a given website and returns astronomical data info
+'''
+
+import requests
+from twit.twitter_bot import TwitterBot
+
+success = {
+    'success': True,
+    'error': False
+}
+fail = {
+    'success':False,
+    'error':True,
+}
+
+class RequestWeb(object):
+    
+    def __init__(self):
+        self.tb = TwitterBot()
+        
+    def _return_msg(self, main, msg):
+        res = main
+        res['msg'] = msg
+        return res
+
+    def determine_response(self, call, args):
+        tar = args['respond_to']
+        if tar == None:
+            return self._return_msg(fail, 'No target user') 
+
+        if call == "sky":
+            # TODO: call the sky scraper
+            self.tb.tweet_at('Hey, you requested sky again', user_scr=tar)
+            return self._return_msg(success, 'tweeted the sky')
+        elif call == "satellite":
+            # TODO: call the satellite scraper
+            self.tb.tweet_at('Hey, you requested satellite', user_scr=tar)
+            return self._return_msg(success, 'tweeted the satellite')
+        else:
+            return self._return_msg(fail, 'unspecified query')
--- wsgi/bot.py
+++ wsgi/bot.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, render_template
+from api.request import RequestWeb as rw
 
 app = Flask(__name__)
    
@@ -6,7 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/api/v1/<path:call>', methods=['GET'])
+def get_data(call):
+    error_msg = {'success':1, 'error':'no'}
+    if request.method == 'GET':
+        return jsonify(rw().determine_response(call, request.args))
+    else:
+        return jsonify(error_msg)
+    
 if __name__ == "__main__":
     app.debug = True
     app.run()
