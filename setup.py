f01ca6aeddd2e72dae6cd5b3d2325e884ea41530
sophia <scastellarin95@gmail.co>
2014-04-13 19:47:09 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='astro_tweet_bot',
       author='Sophia Castellarin, Steven Wu, Alan Wu',
       author_email='binge.coders@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask==0.7.2', 'BeautifulSoup==3.2.1'],
+      install_requires=['Flask==0.7.2', 'BeautifulSoup==4'],
      )
