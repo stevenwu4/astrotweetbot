4e89701e6187a85f95b8d78be9dad50d7c946ed0
sophia <scastellarin95@gmail.co>
2014-04-12 20:30:03 -0400

    get's all the data for accessing tonight sky

--- twitter/get_feed.py
+++ twitter/get_feed.py
@@ -1,10 +1,15 @@
 '''
 Module to get who tweeted at @astro_tweet_bot and then tell the server stuff
+Expected twitter request:
+    For sky
+    @astro_tweet_bot, sky, <start_time> + <length> [, <difficulty>, <info wanted>]
+    For satellite
+    @astro_tweet_bot,  satellite, <postal_code>
 '''
 
 import os
 import twitter
-import re
+import time
 
 CONSUMER_KEY = os.getenv('CONSUMER_KEY')
 CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
@@ -14,26 +19,108 @@ ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 class TwitterBot(object):
 
     def __init__(self):
-       self.api = twitter.Api(consumer_key = CONSUMER_KEY,
+        self.api = twitter.Api(consumer_key = CONSUMER_KEY,
             consumer_secret = CONSUMER_SECRET,
             access_token_key = ACCESS_TOKEN_KEY,
             access_token_secret= ACCESS_TOKEN_SECRET)
-       self.POLL = 1
+        self.POLL = 100
+        self.data_sources_key = ['sky', 'satellite']
 
-    def _determine_freshness(self, time):
+    def _determine_freshness(self, tm):
         # determines if we are going to respond to a tweet
         # params:
         #   time: the time the tweet was sent at
-        print time
-        year = 
-        day = re.search('[0-9].', time).group(0)
+        year = tm[-4:]
+        if time.strftime('%Y') != year:
+            return False
         
+        month = tm[4:7]
+        if time.strftime('%h') != month:
+            return False
+           
+        day = tm[8:10]
+        # greater than because of server offset
+        if int(time.strftime('%d')) > int(day):
+            return False
 
+        hour = tm[11:13]
+        # offset hour b/c of servers
+        if (int(time.strftime('%H'))+4)%24 != int(hour):
+            return False
+
+        mnt = tm[14:16]
+        if int(time.strftime('%M')) > (int(mnt)+self.POLL):
+            return False
+        
+        return True
+    
     def get_feed(self):
         # gets all the mentions
         mentions = self.api.GetMentions()
         for i in mentions:
-            self._determine_freshness(i.created_at)
+            # makes sure it is not an onld tweet   
+            if self._determine_freshness(i.created_at):
+                print i.created_at
+                # checks what kind of info they want
+                if self.get_type(i.text) == 'sky':
+                    self.send_payload_tonightsky(i)
+                elif self.get_type(i.text) == 'satellite':
+                    self.send_payload_satellite(i)
+
+    def get_type(self, text):
+        queries = text.split(',')
+        if len(queries) < 3:
+            return False
+        else:
+            for i in queries:
+                if i.strip() in self.data_sources_key:
+                    return i.strip()
+        return False
+                    
+
+    def send_payload_tonightsky(self, res):
+        payload = {
+            'lat': None,
+            'long': None,
+            'when': None,
+            'length': None,
+            'difficulty': 1,
+            'features': ['Globular Clusters', 'Open Clusters',
+                'Nedula', 'Galaxies', 'Planets', 'Comets',
+                'Asteroids', 'Double Stars', 'Star Group']
+        }
+        # gets the place tag
+        if res.place:
+            sum_lat = 0
+            sum_long = 0
+            box = res.place.get('bounding_box').get('coordinates')[0]
+            for i in box:
+                sum_lat += i[0]
+                sum_long += i[1]
+            payload['lat'] = (sum_lat/4.0)
+            payload['long'] = (sum_long/4.0)
+
+        # gets the geo tag
+        if res.coordinates:
+            payload['lat'] = res.coordinates.get('coordinates')[0]
+            payload['long'] = res.coordinates.get('coordinates')[1]
+        # fills in the rest of the payload
+        queries = res.text.split(',')
+
+        tar_time = queries[2].split('+')
+        if len(tar_time) == 2:
+            payload['when'] = tar_time[0].strip()
+            payload['length'] = tar_time[1].strip()
+        
+        if len(queries) > 3:
+            payload['difficulty'] = queries[3].strip()
+        if len(queries) > 4:
+            payload['features'] = queries[4].split('+')
+
+        print payload
+
+    def send_payload_satellite(self, res):
+        payload = {'postal_code':None}
 
     def tweet_at(self, mssg, user_scr=None, user_id=None):
         # Tweets at a specific user
